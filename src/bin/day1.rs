ae5d9dcd0004f26b8e687da917a763b500a2dfb8
Fred-Jan van der Eijken <fred-jan@vandereijken.nl>
2024-12-01 08:28:30 +0100

    Day 1 part A

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,9 +1,41 @@
 use std::fs;
 
-fn part1() -> usize {
-    let contents = fs::read_to_string("assets/day1/input.txt").expect("Failed to read input");
+struct Input {
+    left: Vec<u32>,
+    right: Vec<u32>,
+}
 
-    10
+impl Input {
+    fn from_string(string: String) -> Self {
+        let (mut left, mut right): (Vec<u32>, Vec<u32>) = string
+            .lines()
+            .map(|line| {
+                let mut ws = line.split_whitespace();
+                (
+                    ws.next().unwrap().parse::<u32>().unwrap(),
+                    ws.next().unwrap().parse::<u32>().unwrap(),
+                )
+            })
+            .unzip();
+
+        left.sort();
+        right.sort();
+
+        Self { left, right }
+    }
+
+    fn part1(&self) -> u32 {
+        self.left
+            .iter()
+            .zip(self.right.iter())
+            .map(|(&a, &b)| a.abs_diff(b))
+            .sum()
+    }
+}
+
+fn part1() -> u32 {
+    Input::from_string(fs::read_to_string("input/day1.txt").expect("Failed to read input"))
+        .part1()
 }
 
 fn main() {
@@ -16,6 +48,18 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(true, true);
+        assert_eq!(
+            11,
+            Input::from_string(
+                "3   4\n\
+                4   3\n\
+                2   5\n\
+                1   3\n\
+                3   9\n\
+                3   3"
+                    .to_string(),
+            )
+            .part1()
+        );
     }
 }
